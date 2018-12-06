4b7b327a05cef389fb78c551c0f1665f481ff47d
AndreV84 <andrevolodin@gmail.com>
2018-12-06 13:22:48 +0000

    Update module.sh
    updated

--- module.sh
+++ module.sh
@@ -3,7 +3,10 @@ cd ~
 git clone https://github.com/jetsonhacks/buildOpenCVXavier
 git clone https://github.com/opencv/opencv_contrib
 git clone https://github.com/opencv/opencv
-gp -r /home/nvidia/opencv_contrib/modules/aruco /home/nvidia/opencv/modules/
-gd buildOpenCVXavier
+git clone https://github.com/AndreV84/ravi
+cp -r /home/nvidia/opencv_contrib/modules/aruco /home/nvidia/opencv/modules/
+rm /home/nvidia/buildOpenCVXavier/buildOpenCV.sh
+cp /home/nvidia/ravi/buildOpenCV.sh /home/nvidia/buildOpenCVXavier/
+cd buildOpenCVXavier
 git checkout v1.0
 sudo ./buildOpenCV.sh
