0c363c1b6cb2ee943c71302bf16ccf92b37f3ec3
AndreV84 <andrevolodin@gmail.com>
2018-12-30 12:12:29 -0500

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 ./ngrok authtoken 6UbXCLHsg35bniyFaLmQD_2WJKezCsKzk7u7AYsZMmE
